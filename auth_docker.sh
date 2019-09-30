e298eae8103d9ad7912f694c3a08b908bd3813c8
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2019-09-30 16:51:01 +1000

    WIP

--- auth_docker.sh
+++ auth_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcloud auth activate-service-account --key-file=${GCLOUD_SERVICE_KEY}
+echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
 gcloud auth configure-docker
