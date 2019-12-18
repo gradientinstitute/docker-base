7daea1a636aa6e95d79a7d5cb92872b480e19133
Lachlan McCalman <lachlan@gradientinstitute.org>
2019-12-18 11:25:58 +1100

    Add google cloud building

--- build_push.sh
+++ build_push.sh
@@ -1,27 +1,27 @@
 #!/bin/bash -e
 
-IMAGE_NAME=python
-FULL_NAME=asia.gcr.io/ongoing-236000/${IMAGE_NAME}
-TAG=$(git log -1 --pretty=%h)
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+# IMAGE_NAME=python
+# FULL_NAME=asia.gcr.io/ongoing-236000/${IMAGE_NAME}
+# TAG=$(git log -1 --pretty=%h)
+# BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-echo "full name: $FULL_NAME"
-echo "tag: $TAG"
-echo "branch: $BRANCH"
+# echo "full name: $FULL_NAME"
+# echo "tag: $TAG"
+# echo "branch: $BRANCH"
 
-echo "building..."
-docker build -t ${IMAGE_NAME} ./python
+# echo "building..."
+# docker build -t ${IMAGE_NAME} ./python
 
-if [[ "$BRANCH" = "master" ]] || [[ "$BRANCH" = "develop"  ]]; then
+# if [[ "$BRANCH" = "master" ]] || [[ "$BRANCH" = "develop"  ]]; then
 
-  echo "tagging and pushing..."
-  docker tag ${IMAGE_NAME} ${FULL_NAME}:${TAG}
-  docker tag ${IMAGE_NAME} ${FULL_NAME}:latest
+#   echo "tagging and pushing..."
+#   docker tag ${IMAGE_NAME} ${FULL_NAME}:${TAG}
+#   docker tag ${IMAGE_NAME} ${FULL_NAME}:latest
 
-  docker push ${FULL_NAME}:${TAG}
-  docker push ${FULL_NAME}:latest
+#   docker push ${FULL_NAME}:${TAG}
+#   docker push ${FULL_NAME}:latest
 
-fi
+# fi
 
 
 
